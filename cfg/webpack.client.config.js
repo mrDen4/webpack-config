8f606355366626e2b695b9647fe25dc08d8e9867
Dennis <mr.den.saint@yandex.ru>
2021-06-09 18:09:57 +0300

    add hmr but with bugs

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const { HotModuleReplacementPlugin } = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
@@ -17,13 +19,20 @@ function setupDevtool() {
 
 module.exports = {
     mode: NODE_ENV ? NODE_ENV : 'development',
-    entry: path.resolve(__dirname, '../src/client/index.tsx'),
+    entry: [
+      path.resolve(__dirname, '../src/client/index.tsx'),
+      'webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr'
+    ],
     output: {
         filename: 'client.js',
+        publicPath: '/static/',
         path: path.resolve(__dirname, '../dist/client'),
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+          'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
+        }
     },
     module: {
         rules: [
@@ -35,4 +44,9 @@ module.exports = {
         ],
     },
     devtool: setupDevtool(),
+    plugins: IS_DEV 
+      ? [
+        new HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+      ] : [],
 };
